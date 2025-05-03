848df40a686e1d991641cd70e59200bb0b77848f
vladislav_dvortsov <dvortsov.vlad@gmail.com>
2025-05-03 18:20:19 +0300

    Публикация

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: '/invite/',
+  },
   css: [
     '~/assets/styles/fonts.scss',
     '~/assets/styles/app.scss',
